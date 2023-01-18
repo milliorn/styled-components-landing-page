29b3e05402037d0171f0552119f89a4ff7b172e9
milliorn <scottmilliorn@gmail.com>
2023-01-17 17:13:09 -0800

    Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import Header from "./components/Header";
 import { Container } from "./components/styles/Container.styled";
 
 function App() {
   return (
-    <Container className="App">
-      <h1>Hello world</h1>
-    </Container>
+    <>
+      <Header />
+      <Container className="App">
+        <h1>Hello world</h1>
+      </Container>
+    </>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyledHeader } from "./styles/Header.styled";
+
+const Header = () => {
+  return (
+    <StyledHeader bg="red">
+      <h1>Hubble</h1>
+    </StyledHeader>
+  );
+};
+
+export default Header;
