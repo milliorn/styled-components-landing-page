8b04d2fc7ce7bb65d6f74726f7d3bfb4f005239e
milliorn <scottmilliorn@gmail.com>
2023-01-17 17:16:10 -0800

    ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import { ThemeProvider } from "styled-components";
 import Header from "./components/Header";
 import { Container } from "./components/styles/Container.styled";
 
+const theme = {
+  colors: { header: "#ebfbff", body: "#fff", footer: "#003333" },
+};
+
 function App() {
   return (
-    <>
-      <Header />
-      <Container className="App">
-        <h1>Hello world</h1>
-      </Container>
-    </>
+    <ThemeProvider theme={theme}>
+      <>
+        <Header />
+        <Container className="App">
+          <h1>Hello world</h1>
+        </Container>
+      </>
+    </ThemeProvider>
   );
 }
 
